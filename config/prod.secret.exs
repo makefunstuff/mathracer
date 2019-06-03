10fd4cf554fa299387f0e6a1bd72e1b2315ee4f3
Iurii Plugatariov <plugatariov@gmail.com>
2019-06-03 17:48:50 +0300

    remove secret from gitignore

---
+++ config/prod.secret.exs
@@ -0,0 +1,12 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you'll likely want to automate and keep away from
+# your version control system.
+#
+# You should document the content of this
+# file or create a script for recreating it, since it's
+# kept out of version control and might be hard to recover
+# or recreate for your teammates (or yourself later on).
+config :mathracer, MathracerWeb.Endpoint,
+  secret_key_base: "fF+CD7DSgR1ui2AlS3668frT3yF61LDFJNGaI//muQ3Ck7qMCsw/7Jn51qPu2E77"
